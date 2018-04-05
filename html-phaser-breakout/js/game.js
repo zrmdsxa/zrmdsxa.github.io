6252571889a9c8911b7abb41242c321ecf774824
Nue <undefinedhoujuunue@gmail.com>
2018-04-05 19:51:44 -0400

    update game

--- html-phaser-breakout/js/game.js
+++ html-phaser-breakout/js/game.js
@@ -23,17 +23,31 @@ var devicePixelRatio;
 
 var background;
 var topbar;
+var leftbar;
+var rightbar;
+var sides;
 var ball;
 var paddle;
-var text;
-var brick;
+var scoreText;
 
 var brickGroup;
+var remainingBricks;
+var startBall;
+
+var ballStartX;
+var ballStartY;
+
+var paddleMinX;
+var paddleMaxX;
+
+var paddleSpeed = 300;
+
 function preload ()
 {
 
 	 this.load.image('sky', 'images/sky.png');
 	 this.load.image('top', 'images/top.png');
+	 this.load.image('sides', 'images/sides.png');
 	 this.load.image('brick', 'images/yellowbrick.png');
 	 this.load.image('ball', 'images/ball.png');
 	 this.load.image('paddle', 'images/paddle.png');
@@ -49,16 +63,40 @@ function preload ()
 function create ()
 {
 	
-	
 	//background
 	background = this.add.image(window.innerWidth/2, window.innerHeight/2, 'sky');
 	background.setScale(devicePixelRatio*4,devicePixelRatio*2);
+
+	
+
+	//sides
+	sides = this.physics.add.staticGroup();
+	sides.create((window.innerWidth/2)-(195 * devicePixelRatio),window.innerHeight/2,'sides').setScale(devicePixelRatio*2,devicePixelRatio*10).refreshBody();
+	sides.create((window.innerWidth/2)+(195 * devicePixelRatio)-(5*devicePixelRatio),window.innerHeight/2,'sides').setScale(devicePixelRatio*2,devicePixelRatio*10).refreshBody();
+
+	var testSide = this.add.image(0,0,'sides');
+	testSide.setScale(devicePixelRatio*2,devicePixelRatio*10);
+
+	var half = testSide.width * devicePixelRatio;
+	console.log(half);
+	testSide.destroy();
+	//var half = testSide
+	
+	//leftbar = this.physics.add.image((window.innerWidth/2)-(195 * devicePixelRatio),window.innerHeight/2,'sides');
+	//leftbar.setScale(devicePixelRatio*2,devicePixelRatio*10);
+	//leftbar.body.immovable = true;
+
+	//rightbar = this.physics.add.image((window.innerWidth/2)+(195 * devicePixelRatio)-(2*devicePixelRatio),window.innerHeight/2,'sides');
+	//rightbar.setScale(devicePixelRatio*2,devicePixelRatio*10);
+	//rightbar.body.immovable = true;
+
+	//sides.add(leftbar);
+	//sides.add(rightbar);
 
 	//top bar
 	topbar = this.physics.add.image(window.innerWidth/2,20 *devicePixelRatio,'top');
 	topbar.setScale(devicePixelRatio*10,devicePixelRatio*2);
 	topbar.body.immovable = true;
-
 
 	//bricks
 	//brickGroup = this.physics.add.staticGroup();
@@ -66,29 +104,29 @@ function create ()
 	console.log(brickGroup);
 	for (var i = 0; i < 5;i++){
 		for (var j = 0; j < 5; j++){
-			//brick = brickGroup.create( window.innerWidth/2 + (i*50*devicePixelRatio - (100*devicePixelRatio)),(devicePixelRatio * 100) +(26*devicePixelRatio * j), 'brick');
+			//					the horizontal spacing between bricks (i*x*dPR), shift should be 2x spacing (2x*dPR)
 			brick = this.physics.add.image(window.innerWidth/2 + (i*50*devicePixelRatio - (100*devicePixelRatio)),(devicePixelRatio * 100) +(26*devicePixelRatio * j), 'brick');
 			brick.setScale(devicePixelRatio * 1.5,devicePixelRatio * 1.5);
-			//brick.body.immovable = true;
 			brickGroup.add(brick);
+			remainingBricks++;
 		}
 		
 		
 	}
+
+	//this makes the bricks static
 	brickGroup.children.iterate(function (child){
 		child.body.immovable = true;
 	});
 	
-
-
-	//brickGroup.add(brick);
-
 	//ball
-	ball = this.physics.add.image(window.innerWidth/2, window.innerHeight - (95 * devicePixelRatio), 'ball');
+	ballStartX = window.innerWidth/2;
+	ballStartY = window.innerHeight - (95 * devicePixelRatio);
+	ball = this.physics.add.image(ballStartX, ballStartY, 'ball');
 	ball.setBounce(1.0);
-	ball.setCollideWorldBounds(true);
-	ball.body.setVelocityY(300 * devicePixelRatio);
-	ball.setScale(devicePixelRatio,devicePixelRatio);
+	//ball.setCollideWorldBounds(true);
+	//
+	ball.setScale(devicePixelRatio * 0.8,devicePixelRatio * 0.8);
 
 	//paddle
 	paddle = this.physics.add.image(window.innerWidth/2, window.innerHeight - (40 * devicePixelRatio) , 'paddle');
@@ -96,32 +134,103 @@ function create ()
 	paddle.body.immovable = true;
 	paddle.setScale(devicePixelRatio,devicePixelRatio);
 
+	paddleMinX = (window.innerWidth/2)-(195 * devicePixelRatio) + half + (paddle.width/2 * devicePixelRatio);
+	paddleMaxX = (window.innerWidth/2)+(195 * devicePixelRatio)-(2*devicePixelRatio) - half - (paddle.width/2 * devicePixelRatio);
+
+	console.log(ball.x);
+	console.log(paddle.x);
+	console.log(window.innerWidth/2);
+
+	//collision detections
+	//this.physics.add.collider(paddle, leftbar);
+	//this.physics.add.collider(paddle, rightbar);
+
 	this.physics.add.collider(ball, topbar);
+	//this.physics.add.collider(ball, leftbar);
+	//this.physics.add.collider(ball, rightbar);
+	this.physics.add.collider(ball, sides);
+	this.physics.add.collider(paddle, sides);
 
 	this.physics.add.collider(paddle,ball,hitBall,null,this);
-
 	this.physics.add.collider(brickGroup,ball,hitBrick,null,this);
-	//text = this.add.text(9, 9, 'test:x', { font: "20px Arial", fill: "#ffffff", align: "left" });
+	
 
+
+	scoreText = this.add.text(9, 9, 'Score: 0', { font: 20 * devicePixelRatio+"px Arial", fill: "#ffffff", align: "left" });
+
+	score = 0;
+	startBall = true;
+
+	//ball.body.setVelocityY(300 * devicePixelRatio);
+	
+	this.input.on('pointerdown',onPointerDown,this);
+	//this.input.onTap.add(this.onTap,this);
 }
 
 function update ()
 {
 	if (this.input.activePointer.isDown){
-		//console.log(this.input.x);
+
+		//console.log(this.input.x - paddle.x);
 		if (this.input.x <= paddle.x){
-			paddle.body.setVelocityX(-250 * devicePixelRatio);
+			if ((this.input.x - paddle.x) > -5){
+				paddle.body.setVelocityX(0);
+				paddle.x = this.input.x;
+				
+			}
+			else {
+				paddle.body.setVelocityX(-250 * devicePixelRatio);
+			}
 			//console.log("left");
 		}	
 		else {
-			paddle.body.setVelocityX(250 * devicePixelRatio);
+			if ((this.input.x - paddle.x) < 5){
+				paddle.body.setVelocityX(0);
+				paddle.x = this.input.x;
+				
+			}
+			else {
+				paddle.body.setVelocityX(250 * devicePixelRatio);
+			}
 			//console.log("right");
 		}
 		
 	}
+	
 	else{
 		paddle.body.setVelocityX(0);
 	}
+
+
+	if(ball.y >= window.innerHeight){
+		startBall = true;
+
+		ball.body.setVelocityX(0);
+		ball.body.setVelocityY(0);
+
+		paddle.x = ballStartX;
+		ball.x = ballStartX;
+		ball.y = ballStartY;
+
+		score = 0;
+		scoreText.setText('Score: '+score);
+
+		
+	}
+
+	if(paddle.x < paddleMinX){
+		paddle.x = paddleMinX;
+	}
+	else if(paddle.x > paddleMaxX){
+		paddle.x = paddleMaxX;
+	}
+
+	if(startBall){
+		paddle.x = ballStartX;
+		ball.x = ballStartX;
+		ball.y = ballStartY;
+	}
+
 }
 
 function hitBall (paddle,ball){
@@ -134,6 +243,18 @@ function hitBall (paddle,ball){
 }
 
 function hitBrick(ball,brick){
-	//brick.disableBody(true,true);
+	score += 1;
+	scoreText.setText('Score: '+score);
+	remainingBricks--;
 	brick.destroy();
+
+}
+
+function onPointerDown(pointer, gameObjects){
+	console.log("pointerdown");
+	if(startBall){
+		startBall = false;
+
+		ball.body.setVelocityY(300 * devicePixelRatio);
+	}
 }
